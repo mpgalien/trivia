5383f11e3d02c716c287d1e77fc626e6bf5bae9f
Mitchell van der Galiën <mpgalien@gmail.com>
2021-07-27 14:43:59 +0200

    Make some improvements to the tests of 'isPlayable' and 'add' functions

--- game.spec.js
+++ game.spec.js
@@ -10,18 +10,30 @@ describe("The test environment", function() {
   });
 });
 
-describe("Your specs...", function() {
-  it("should be playable after adding 2 players or more", function() {
+describe("Playing the game", function() {
+  it("should be able to add a player", function () {
+    var game = new Game();
+
+    const addedPlayer = game.add('New player');
+    expect(addedPlayer).toBe(true);
+  })
+  it("should not be playable with 0 or 1 player", function() {
     var game = new Game();
 
     expect(game.isPlayable()).toBe(false);
 
-    const addedPlayer1 = game.add('Player 1');
-    expect(addedPlayer1).toBe(true);
+    game.add('Player 1');
     expect(game.isPlayable()).toBe(false);
+  })
+  it("should be playable with 2 or more players", function () {
+    var game = new Game();
 
-    const addedPlayer2 = game.add('Player 2');
-    expect(addedPlayer2).toBe(true);
+    game.add('Player 1');
+    game.add('Player 2');
     expect(game.isPlayable()).toBe(true);
+    
+    game.add('Player 3');
+    expect(game.isPlayable()).toBe(true);
+
   })
 });
